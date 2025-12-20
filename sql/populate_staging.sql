c074caf4730e3e0616a419c2b94e99b5ed51eee8
Subrata Sarker <subrata_sarker@yahoo.com>
2025-12-19 21:38:52 -0500

    Remove script for estimating cost and profit

--- sql/populate_staging.sql
+++ sql/populate_staging.sql
@@ -15,37 +15,6 @@ COPY staging_sales_raw (
 FROM '../data/retail_sales_50krows.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
--- Populate new-derived columns in staging_sales
-UPDATE staging_sales_raw
-SET
-	sale_year 		= EXTRACT(YEAR FROM sale_date)::INTEGER,
-	sale_month 		= EXTRACT(MONTH FROM sale_date)::INTEGER,
-	sale_quarter	= EXTRACT(QUARTER FROM sale_date)::INTEGER,
-	weekday 		= TO_CHAR(sale_date, 'Day'); -- e.g., 'Monday'
-
--- Remove whitespaces from weekday
-UPDATE staging_sales_raw
-SET weekday = TRIM(weekday);
-
--- Insert default cost factors
-INSERT INTO product_margin (category, cost_factor) VALUES
--- The arbitrary cost_factors can be replaced with real
--- cost factors or the whole table can be generated from real data
-    ('Electronics', 0.62),
-    ('Accessories', 0.82),
-    ('Other', 0.70)
-ON CONFLICT (category) DO NOTHING;
-
--- Populate estimated_cost and estimated_profit 
--- columns in table staging_sales
-UPDATE staging_sales_raw s
-SET estimated_cost = 
-		s.total_sales * m.cost_factor,
-	estimated_profit = 
-		s.total_sales * (1 - m.cost_factor)
-FROM product_margin m
-WHERE s.category = m.category;
-
 
 SELECT COUNT(sale_date) AS total_count FROM staging_sales_raw;
 -- pgsql> 50000
