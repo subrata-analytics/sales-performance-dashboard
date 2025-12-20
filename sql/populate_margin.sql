85b026076ab9880db4af19267722ff67c16fe013
Subrata Sarker <subrata_sarker@yahoo.com>
2025-12-20 12:22:35 -0500

    Remove Other category from product_margin

--- sql/populate_margin.sql
+++ sql/populate_margin.sql
@@ -3,6 +3,5 @@ INSERT INTO product_margin (category, cost_factor) VALUES
 -- The arbitrary cost_factors can be replaced with real
 -- cost factors or the whole table can be generated from real data
     ('Electronics', 0.65),
-    ('Accessories', 0.65),
-    ('Other', 0.65)
+    ('Accessories', 0.65)
 ON CONFLICT (category) DO NOTHING;
